d7490d7619779cd952b3a201b9710fe72dbf4840
DoGamerYT <144898938+DoGamerYT@users.noreply.github.com>
2025-03-19 12:30:50 +0100

    Tutorial 3

--- ArcadeM3/Assets/Scripts/Movement.cs
+++ ArcadeM3/Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;      
+    public float jumpForce = 7f;      
+    public LayerMask groundLayer;     
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        CheckGrounded();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal"); 
+        float moveZ = Input.GetAxis("Vertical");   
+
+        Vector3 moveDirection = new Vector3(moveX, 0f, moveZ) * moveSpeed;
+        Vector3 newVelocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+
+        rb.velocity = newVelocity;
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
     }
 }
